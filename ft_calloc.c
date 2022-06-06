6281b5a2c5d5b1fb7021a7ea80ae89028ed85617
Chong Weng hong <cweng-ho@u90z03s05.42kl.edu.my>
2022-06-06 12:24:24 +0800

    New

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: cweng-ho <chong970620@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 13:50:07 by cweng-ho          #+#    #+#             */
-/*   Updated: 2022/06/01 18:12:28 by cweng-ho         ###   ########.fr       */
+/*   Updated: 2022/06/06 12:23:37 by cweng-ho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void    *ft_calloc(size_t count, size_t size)
 {
     void    *ptr;
 
-    ptr = malloc(count * size);
+    ptr = (void *)malloc(count * size);
     if(!ptr)
-        return (0);
+        return (NULL);
     ft_bzero(ptr, count);
     return (ptr);
 }
